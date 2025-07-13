6f2ff19e6fb2ccf23c794130e00837839d38f499
Rolling Banana <102475553+chuyennguyenn@users.noreply.github.com>
2025-07-13 13:40:22 -0500

    add control instructions

--- FlappyBird 2.0/Assets/Script/UIManager/MainMenuManager.cs
+++ FlappyBird 2.0/Assets/Script/UIManager/MainMenuManager.cs
@@ -2,6 +2,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour
 {
+
+    public GameObject ControlScene;
+
+    public GameObject MainMenuScene;
+
+    public bool controlsActive = false;
+
+    private void Start()
+    {
+        MainMenuScene.SetActive(true); // Ensure the main menu scene is active at the start
+        controlsActive = false; // Initialize controlsActive to false
+        ControlScene.SetActive(false); // Ensure the controls menu is hidden at the start
+    }
     public void StartGame(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
@@ -10,5 +23,20 @@ public class MainMenuManager : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit(); // Quit the application
+    }
+
+    public void ControlMenu()
+    {
+        controlsActive = !controlsActive; // Toggle the controls menu visibility
+        if (controlsActive)
+        {
+            ControlScene.SetActive(true); // Show the controls menu
+            MainMenuScene.SetActive(false); // Hide the main menu scene
+        }
+        else
+        {
+            ControlScene.SetActive(false); // Hide the controls menu
+            MainMenuScene.SetActive(true);
+        }
     }
 }
